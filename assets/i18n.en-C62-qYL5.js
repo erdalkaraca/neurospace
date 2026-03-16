975a575c36f23a196ffd092e1b7310087cdfac10
erdalkaraca <erdalkaraca@users.noreply.github.com>
2026-03-16 17:20:15 +0000

    deploy: 25ef21ea1c148a9db89da2fad295d26f86cf4fa9

---
+++ assets/i18n.en-C62-qYL5.js
@@ -0,0 +1,1 @@
+const E="SNIRF Viewer",t="View fNIRS data in SNIRF format",I={EXT_SNIRFVIEWER_NAME:E,EXT_SNIRFVIEWER_DESC:t};export{t as EXT_SNIRFVIEWER_DESC,E as EXT_SNIRFVIEWER_NAME,I as default};
